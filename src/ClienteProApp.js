e6dab8183ce53a578e26faae5f95a5d5af28f192
guidomatos <guido.matos.88@gmail.com>
2021-02-01 18:05:17 -0500

    Creacion de Proyecto Cliente PRO

---
+++ src/ClienteProApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ClienteProApp = () =>  {
+
+    return (
+        <>
+            <h1>ClientePro App</h1>
+        </>
+    );
+
+}
+
+export default ClienteProApp;
